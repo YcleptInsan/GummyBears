4b63d1b6ce6e8b7d56416b5483d77941d8e1622d
YcleptInsan <nickmwise@gmail.com>
2017-08-04 10:08:29 -0700

    Making class for Object to connect with SSMS Database

---
+++ WebApplication2/Models/GummyBearContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GummyBear.Models
+{
+    public class ToDoListContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GummyBear;integrated security=True");
+        }
+    }
+}
---
+++ WebApplication2/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummyBear.Models
+{
+    public class Class1
+    {
+        [Table("Products")]
+        public class Product
+        {
+            [Key]
+            public int ProductId { get; set; }
+            public string Name { get; set; }
+            public string Cost { get; set; }
+            public string Country { get; set; }
+        }
+    }
+}
